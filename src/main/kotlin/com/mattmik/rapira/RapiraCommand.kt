9c721b10cb7ce8de12cda4b3d9e5d83de3de48cf
Matthew Mikolay <mikolaym@yahoo.com>
2020-04-16 12:32:07 -0400

    Process input using generated lexer and parser

--- src/main/kotlin/com/mattmik/rapira/RapiraCommand.kt
+++ src/main/kotlin/com/mattmik/rapira/RapiraCommand.kt
@@ -4,6 +4,10 @@ import com.github.ajalt.clikt.core.CliktCommand
 import com.github.ajalt.clikt.parameters.arguments.argument
 import com.github.ajalt.clikt.parameters.options.versionOption
 import com.github.ajalt.clikt.parameters.types.file
+import com.mattmik.rapira.antlr.RapiraLangLexer
+import com.mattmik.rapira.antlr.RapiraLangParser
+import org.antlr.v4.runtime.CharStreams
+import org.antlr.v4.runtime.CommonTokenStream
 
 class RapiraCommand : CliktCommand(
     name = "rapira",
@@ -17,7 +21,10 @@ class RapiraCommand : CliktCommand(
     }
 
     override fun run() {
-        echo("Received file ${inputFile.path}")
+        inputFile.inputStream().use {
+            val lexer = RapiraLangLexer(CharStreams.fromStream(it))
+            RapiraLangParser(CommonTokenStream(lexer)).program()
+        }
     }
 }
 
