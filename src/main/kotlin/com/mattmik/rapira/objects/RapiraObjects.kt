61ac7d4c2c80a0da912875fa0435501c8e923cb5
Matthew Mikolay <mikolaym@yahoo.com>
2020-04-19 11:21:32 -0400

    Add placeholder RapiraEmpty object

--- src/main/kotlin/com/mattmik/rapira/objects/RapiraObjects.kt
+++ src/main/kotlin/com/mattmik/rapira/objects/RapiraObjects.kt
@@ -5,12 +5,26 @@ sealed class RapiraObject {
     abstract fun subtract(other: RapiraObject): RapiraObject
 }
 
+object RapiraEmpty : RapiraObject() {
+    override fun add(other: RapiraObject) = when (other) {
+        is RapiraInteger -> TODO("Not yet implemented")
+        is RapiraEmpty -> TODO("Not yet implemented")
+    }
+
+    override fun subtract(other: RapiraObject) = when (other) {
+        is RapiraInteger -> TODO("Not yet implemented")
+        is RapiraEmpty -> TODO("Not yet implemented")
+    }
+}
+
 data class RapiraInteger(val value: Int) : RapiraObject() {
     override fun add(other: RapiraObject) = when (other) {
         is RapiraInteger -> RapiraInteger(value + other.value)
+        is RapiraEmpty -> TODO("Not yet implemented")
     }
 
     override fun subtract(other: RapiraObject) = when (other) {
         is RapiraInteger -> RapiraInteger(value - other.value)
+        is RapiraEmpty -> TODO("Not yet implemented")
     }
 }
