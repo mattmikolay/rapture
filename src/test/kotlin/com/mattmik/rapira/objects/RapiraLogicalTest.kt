49b09301bf8bdab3db3d0a0c1a2097ad8bfcd182
Matthew Mikolay <mikolaym@yahoo.com>
2020-04-24 09:54:13 -0400

    Use StringSpec in RapiraLogicalTest

--- src/test/kotlin/com/mattmik/rapira/objects/RapiraLogicalTest.kt
+++ src/test/kotlin/com/mattmik/rapira/objects/RapiraLogicalTest.kt
@@ -1,85 +1,69 @@
 package com.mattmik.rapira.objects
 
 import com.mattmik.rapira.errors.RapiraInvalidOperationError
-import org.junit.jupiter.api.Assertions
-import org.junit.jupiter.api.Test
-import org.junit.jupiter.api.assertThrows
+import io.kotest.assertions.throwables.shouldThrow
+import io.kotest.core.spec.style.StringSpec
+import io.kotest.data.forAll
+import io.kotest.data.row
+import io.kotest.matchers.shouldBe
+import io.kotest.property.checkAll
 
-class RapiraLogicalTest {
+class RapiraLogicalTest : StringSpec({
 
-    private val trueLogical = RapiraLogical(true)
-    private val falseLogical = RapiraLogical(false)
-    private val incompatibleObjects = listOf(
-        RapiraEmpty,
-        RapiraProcedure,
-        RapiraFunction,
-        RapiraInteger(1),
-        RapiraReal(1.0),
-        RapiraText("hello"),
-        RapiraSequence()
-    )
-
-    @Test
-    fun andWithLogicalReturnsLogical() {
-        assertEquals(
-            trueLogical,
-            trueLogical.and(trueLogical)
-        )
-        assertEquals(
-            falseLogical,
-            trueLogical.and(falseLogical)
-        )
-        assertEquals(
-            falseLogical,
-            falseLogical.and(trueLogical)
-        )
-        assertEquals(
-            falseLogical,
-            falseLogical.and(falseLogical)
-        )
+    "and with logical returns logical" {
+        checkAll<Boolean, Boolean> { a, b ->
+            RapiraLogical(a).and(RapiraLogical(b)) shouldBe RapiraLogical(a && b)
+        }
     }
 
-    @Test
-    fun andWithOtherTypesThrowsException() = incompatibleObjects.forEach {
-        assertThrows<RapiraInvalidOperationError> { trueLogical.and(it) }
-        assertThrows<RapiraInvalidOperationError> { falseLogical.and(it) }
+    "and with other types throws exception" {
+        val trueLogical = RapiraLogical(true)
+        val falseLogical = RapiraLogical(false)
+        forAll(
+            row(RapiraEmpty),
+            row(RapiraProcedure),
+            row(RapiraFunction),
+            row(RapiraInteger(1)),
+            row(RapiraReal(1.0)),
+            row(RapiraText("hello")),
+            row(RapiraSequence())
+        ) { obj ->
+            shouldThrow<RapiraInvalidOperationError> { trueLogical.and(obj) }
+            shouldThrow<RapiraInvalidOperationError> { falseLogical.and(obj) }
+        }
     }
 
-    @Test
-    fun orWithLogicalReturnsLogical() {
-        assertEquals(
-            trueLogical,
-            trueLogical.or(trueLogical)
-        )
-        assertEquals(
-            trueLogical,
-            trueLogical.or(falseLogical)
-        )
-        assertEquals(
-            trueLogical,
-            falseLogical.or(trueLogical)
-        )
-        assertEquals(
-            falseLogical,
-            falseLogical.or(falseLogical)
-        )
+    "or with logical returns logical" {
+        checkAll<Boolean, Boolean> { a, b ->
+            RapiraLogical(a).or(RapiraLogical(b)) shouldBe RapiraLogical(a || b)
+        }
     }
 
-    @Test
-    fun orWithOtherTypesThrowsException() = incompatibleObjects.forEach {
-        assertThrows<RapiraInvalidOperationError> { trueLogical.or(it) }
-        assertThrows<RapiraInvalidOperationError> { falseLogical.or(it) }
+    "or with other types throws exception" {
+        val trueLogical = RapiraLogical(true)
+        val falseLogical = RapiraLogical(false)
+        forAll(
+            row(RapiraEmpty),
+            row(RapiraProcedure),
+            row(RapiraFunction),
+            row(RapiraInteger(1)),
+            row(RapiraReal(1.0)),
+            row(RapiraText("hello")),
+            row(RapiraSequence())
+        ) { obj ->
+            shouldThrow<RapiraInvalidOperationError> { trueLogical.or(obj) }
+            shouldThrow<RapiraInvalidOperationError> { falseLogical.or(obj) }
+        }
     }
 
-    @Test
-    fun notReturnsOppositeLogical() {
-        assertEquals(trueLogical, falseLogical.not())
-        assertEquals(falseLogical, trueLogical.not())
+    "not with logical returns logical" {
+        checkAll<Boolean> { a ->
+            RapiraLogical(a).not() shouldBe RapiraLogical(!a)
+        }
     }
 
-    @Test
-    fun toStringReturnsUserFriendlyRepresentation() {
-        Assertions.assertEquals("yes", trueLogical.toString())
-        Assertions.assertEquals("no", falseLogical.toString())
+    "toString returns user friendly representation" {
+        RapiraLogical(true).toString() shouldBe "yes"
+        RapiraLogical(false).toString() shouldBe "no"
     }
-}
+})
