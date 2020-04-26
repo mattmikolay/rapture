6ab78d1736a7c4a54bbdd21234dbdc9dba156bb8
Matthew Mikolay <mikolaym@yahoo.com>
2020-04-26 16:23:24 -0400

    Update RapiraIntegerTest

--- src/main/kotlin/com/mattmik/rapira/objects/ObjectUtils.kt
+++ src/main/kotlin/com/mattmik/rapira/objects/ObjectUtils.kt
@@ -3,10 +3,9 @@ package com.mattmik.rapira.objects
 const val ESCAPE_SEQUENCE = "\"\""
 
 fun parseEscapedText(escapedText: String) =
-    RapiraText(
-        escapedText.substring(1, escapedText.length - 1)
-            .replace(ESCAPE_SEQUENCE, "\"")
-    )
+    escapedText.substring(1, escapedText.length - 1)
+        .replace(ESCAPE_SEQUENCE, "\"")
+        .toRapiraText()
 
 /**
  * Returns a formatted string representation of a [RapiraObject] for use with
--- src/test/kotlin/com/mattmik/rapira/objects/RapiraIntegerTest.kt
+++ src/test/kotlin/com/mattmik/rapira/objects/RapiraIntegerTest.kt
@@ -4,80 +4,79 @@ import com.mattmik.rapira.errors.RapiraInvalidOperationError
 import io.kotest.core.spec.style.StringSpec
 import io.kotest.matchers.shouldBe
 import io.kotest.property.checkAll
-import io.kotest.property.forAll
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.assertThrows
 
 class NewRapiraIntegerTest : StringSpec({
     "addition with integer returns integer" {
-        forAll<Int, Int> { a, b ->
-            RapiraInteger(a).add(RapiraInteger(b)) == RapiraInteger(a + b)
+        checkAll<Int, Int> { a, b ->
+            a.toRapiraInteger().add(b.toRapiraInteger()) shouldBe (a + b).toRapiraInteger()
         }
     }
 
     "addition with real returns real" {
-        forAll<Int, Double> { a, b ->
-            RapiraInteger(a).add(RapiraReal(b)) == RapiraReal(a + b)
+        checkAll<Int, Double> { a, b ->
+            a.toRapiraInteger().add(b.toRapiraReal()) shouldBe (a + b).toRapiraReal()
         }
     }
 
     "subtraction with integer returns integer" {
-        forAll<Int, Int> { a, b ->
-            RapiraInteger(a).subtract(RapiraInteger(b)) == RapiraInteger(a - b)
+        checkAll<Int, Int> { a, b ->
+            a.toRapiraInteger().subtract(b.toRapiraInteger()) shouldBe (a - b).toRapiraInteger()
         }
     }
 
     "subtraction with real returns real" {
-        forAll<Int, Double> { a, b ->
-            RapiraInteger(a).subtract(RapiraReal(b)) == RapiraReal(a - b)
+        checkAll<Int, Double> { a, b ->
+            a.toRapiraInteger().subtract(b.toRapiraReal()) shouldBe (a - b).toRapiraReal()
         }
     }
 
     "less than with integer returns logical" {
-        forAll<Int, Int> { a, b ->
-            RapiraInteger(a).lessThan(RapiraInteger(b)) == RapiraLogical(a < b)
+        checkAll<Int, Int> { a, b ->
+            a.toRapiraInteger().lessThan(b.toRapiraInteger()) shouldBe RapiraLogical(a < b)
         }
     }
 
     "less than with real returns logical" {
-        forAll<Int, Double> { a, b ->
-            RapiraInteger(a).lessThan(RapiraReal(b)) == RapiraLogical(a < b)
+        checkAll<Int, Double> { a, b ->
+            a.toRapiraInteger().lessThan(b.toRapiraReal()) shouldBe RapiraLogical(a < b)
         }
     }
 
     "less than or equal to with integer returns logical" {
-        forAll<Int, Int> { a, b ->
-            RapiraInteger(a).lessThanEqualTo(RapiraInteger(b)) == RapiraLogical(a <= b)
+        checkAll<Int, Int> { a, b ->
+            a.toRapiraInteger().lessThanEqualTo(b.toRapiraInteger()) shouldBe RapiraLogical(a <= b)
         }
     }
 
     "less than or equal to with real returns logical" {
-        forAll<Int, Double> { a, b ->
-            RapiraInteger(a).lessThanEqualTo(RapiraReal(b)) == RapiraLogical(a <= b)
+        checkAll<Int, Double> { a, b ->
+            a.toRapiraInteger().lessThanEqualTo(b.toRapiraReal()) shouldBe RapiraLogical(a <= b)
         }
     }
 
     "greater than with integer returns logical" {
-        forAll<Int, Int> { a, b ->
-            RapiraInteger(a).greaterThan(RapiraInteger(b)) == RapiraLogical(a > b)
+        checkAll<Int, Int> { a, b ->
+            a.toRapiraInteger().greaterThan(b.toRapiraInteger()) shouldBe RapiraLogical(a > b)
         }
     }
 
     "greater than with real returns logical" {
-        forAll<Int, Double> { a, b ->
-            RapiraInteger(a).greaterThan(RapiraReal(b)) == RapiraLogical(a > b)
+        checkAll<Int, Double> { a, b ->
+            a.toRapiraInteger().greaterThan(b.toRapiraReal()) shouldBe RapiraLogical(a > b)
         }
     }
 
     "greater than or equal to with integer returns logical" {
-        forAll<Int, Int> { a, b ->
-            RapiraInteger(a).greaterThanEqualTo(RapiraInteger(b)) == RapiraLogical(a >= b)
+        checkAll<Int, Int> { a, b ->
+            a.toRapiraInteger().greaterThanEqualTo(b.toRapiraInteger()) shouldBe RapiraLogical(a >= b)
         }
     }
 
     "greater than or equal to with real returns logical" {
-        forAll<Int, Double> { a, b ->
-            RapiraInteger(a).greaterThanEqualTo(RapiraReal(b)) == RapiraLogical(a >= b)
+        checkAll<Int, Double> { a, b ->
+            a.toRapiraInteger().greaterThanEqualTo(b.toRapiraReal()) shouldBe RapiraLogical(a >= b)
         }
     }
 
@@ -89,7 +88,7 @@ class NewRapiraIntegerTest : StringSpec({
 
     // "modulus with integer returns integer" {
     //     forAll<Int, Int> { a, b ->
-    //         RapiraInteger(a).modulus(RapiraInteger(b)) == RapiraInteger(a % b)
+    //         RapiraInteger(a).modulus(b.toRapiraInteger()) == RapiraInteger(a % b)
     //     }
     // }
 
