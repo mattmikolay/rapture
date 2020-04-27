0f6b44c36ef69ae3fd82cf7a6af9cb7f94054f9a
Matthew Mikolay <mikolaym@yahoo.com>
2020-04-27 19:50:31 -0400

    Add special values to Environment

--- src/main/kotlin/com/mattmik/rapira/Environment.kt
+++ src/main/kotlin/com/mattmik/rapira/Environment.kt
@@ -1,14 +1,25 @@
 package com.mattmik.rapira
 
+import com.mattmik.rapira.errors.RapiraInvalidOperationError
 import com.mattmik.rapira.objects.RapiraEmpty
+import com.mattmik.rapira.objects.RapiraLogical
 import com.mattmik.rapira.objects.RapiraObject
+
+private val specialValues = mapOf(
+    "empty" to RapiraEmpty,
+    "yes" to RapiraLogical(true),
+    "no" to RapiraLogical(false)
+)
 
 class Environment {
     private val bindings = mutableMapOf<String, RapiraObject>()
 
     fun setObject(name: String, value: RapiraObject) {
+        if (specialValues.containsKey(name)) {
+            throw RapiraInvalidOperationError("Cannot overwrite reserved word $name")
+        }
         bindings[name] = value
     }
 
-    fun getObject(name: String) = bindings.getOrDefault(name, RapiraEmpty)
+    fun getObject(name: String) = specialValues[name] ?: bindings.getOrDefault(name, RapiraEmpty)
 }
--- src/test/kotlin/com/mattmik/rapira/EnvironmentTest.kt
+++ src/test/kotlin/com/mattmik/rapira/EnvironmentTest.kt
@@ -1,20 +1,47 @@
 package com.mattmik.rapira
 
+import com.mattmik.rapira.errors.RapiraInvalidOperationError
 import com.mattmik.rapira.objects.RapiraEmpty
 import com.mattmik.rapira.objects.RapiraInteger
-import io.kotest.core.spec.style.StringSpec
+import com.mattmik.rapira.objects.RapiraLogical
+import io.kotest.assertions.throwables.shouldThrow
+import io.kotest.core.spec.style.WordSpec
 import io.kotest.matchers.shouldBe
 
-class EnvironmentTest : StringSpec({
-    "getObject returns empty as default" {
-        val environment = Environment()
-        environment.getObject("not_present") shouldBe RapiraEmpty
+class EnvironmentTest : WordSpec({
+    "getObject" should {
+        "return empty as default" {
+            val environment = Environment()
+            environment.getObject("not_present") shouldBe RapiraEmpty
+        }
+
+        "return special values" {
+            val environment = Environment()
+            environment.getObject("empty") shouldBe RapiraEmpty
+            environment.getObject("yes") shouldBe RapiraLogical(true)
+            environment.getObject("no") shouldBe RapiraLogical(false)
+        }
     }
 
-    "objects can be stored and retrieved" {
-        val environment = Environment()
-        val obj = RapiraInteger(123)
-        environment.setObject("abc", obj)
-        environment.getObject("abc") shouldBe obj
+    "setObject" should {
+        "store custom objects" {
+            val environment = Environment()
+            val obj = RapiraInteger(123)
+            environment.setObject("abc", obj)
+            environment.getObject("abc") shouldBe obj
+        }
+
+        "throw exception with reserved names" {
+            val environment = Environment()
+            listOf(
+                "empty",
+                "yes",
+                "no"
+            ).forEach { name ->
+                shouldThrow<RapiraInvalidOperationError> {
+                    environment.setObject(name, RapiraEmpty)
+                }
+            }
+        }
     }
 })
