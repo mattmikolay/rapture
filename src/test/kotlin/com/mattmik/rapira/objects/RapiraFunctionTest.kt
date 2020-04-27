dbbd092e932060afbbad45e26861c03effd4d585
Matthew Mikolay <mikolaym@yahoo.com>
2020-04-27 09:58:44 -0400

    Update UTs for procedures and functions

--- src/test/kotlin/com/mattmik/rapira/objects/RapiraFunctionTest.kt
+++ src/test/kotlin/com/mattmik/rapira/objects/RapiraFunctionTest.kt
@@ -1,9 +1,28 @@
 package com.mattmik.rapira.objects
 
-import io.kotest.core.spec.style.StringSpec
+import com.mattmik.rapira.Environment
+import com.mattmik.rapira.errors.RapiraInvalidOperationError
+import io.kotest.assertions.throwables.shouldThrow
+import io.kotest.core.spec.style.WordSpec
 
-class RapiraFunctionTest : StringSpec({
-    "toString returns user friendly representation" {
-        RapiraFunction() shouldConvertToString "function"
+class RapiraFunctionTest : WordSpec({
+    "call" should {
+        "throw exception when param and argument count differ" {
+            val params = listOf("param1", "param2", "param3")
+            val arguments = listOf(
+                "arg1".toRapiraText(),
+                "arg2".toRapiraText()
+            )
+            val function = RapiraFunction(null, params)
+            shouldThrow<RapiraInvalidOperationError> {
+                function.call(Environment(), arguments)
+            }
+        }
+    }
+
+    "toString" should {
+        "return user friendly representation" {
+            RapiraFunction() shouldConvertToString "function"
+        }
     }
 })
--- src/test/kotlin/com/mattmik/rapira/objects/RapiraProcedureTest.kt
+++ src/test/kotlin/com/mattmik/rapira/objects/RapiraProcedureTest.kt
@@ -1,9 +1,28 @@
 package com.mattmik.rapira.objects
 
-import io.kotest.core.spec.style.StringSpec
+import com.mattmik.rapira.Environment
+import com.mattmik.rapira.errors.RapiraInvalidOperationError
+import io.kotest.assertions.throwables.shouldThrow
+import io.kotest.core.spec.style.WordSpec
 
-class RapiraProcedureTest : StringSpec({
-    "toString returns user friendly representation" {
-        RapiraProcedure() shouldConvertToString "procedure"
+class RapiraProcedureTest : WordSpec({
+    "call" should {
+        "throw exception when param and argument count differ" {
+            val params = listOf("param1", "param2", "param3")
+            val arguments = listOf(
+                "arg1".toRapiraText(),
+                "arg2".toRapiraText()
+            )
+            val procedure = RapiraProcedure(null, params)
+            shouldThrow<RapiraInvalidOperationError> {
+                procedure.call(Environment(), arguments)
+            }
+        }
+    }
+
+    "toString" should {
+        "return user friendly representation" {
+            RapiraProcedure() shouldConvertToString "procedure"
+        }
     }
 })
