427e61276c4becc3281dc66ad4641d9a9737f3e4
Matthew Mikolay <mikolaym@yahoo.com>
2020-05-05 10:48:49 -0400

    Fix IndexedVariable set logic

--- src/main/kotlin/com/mattmik/rapira/variables/IndexedVariable.kt
+++ src/main/kotlin/com/mattmik/rapira/variables/IndexedVariable.kt
@@ -1,7 +1,9 @@
 package com.mattmik.rapira.variables
 
+import com.mattmik.rapira.errors.RapiraInvalidOperationError
 import com.mattmik.rapira.objects.RInteger
 import com.mattmik.rapira.objects.RObject
+import com.mattmik.rapira.objects.Text
 import com.mattmik.rapira.objects.toSequence
 
 class IndexedVariable(
@@ -12,9 +14,21 @@ class IndexedVariable(
     override var value: RObject
         get() = variable.value.elementAt(index)
         set(value) {
-            // TODO Look at quirks of index assignment, p24
-            variable.value = variable.value.slice(start = null, end = index.minus(RInteger(1))) +
-                listOf(value).toSequence() +
-                variable.value.slice(start = index.plus(RInteger(1)), end = null)
+            when (variable.value) {
+                is Text -> {
+                    if (value !is Text || value.length() != RInteger(1)) {
+                        throw RapiraInvalidOperationError("Must pass text of length 1 to index assignment")
+                    }
+
+                    variable.value = variable.value.slice(start = null, end = index.minus(RInteger(1))) +
+                        value +
+                        variable.value.slice(start = index.plus(RInteger(1)), end = null)
+                }
+                else -> {
+                    variable.value = variable.value.slice(start = null, end = index.minus(RInteger(1))) +
+                        listOf(value).toSequence() +
+                        variable.value.slice(start = index.plus(RInteger(1)), end = null)
+                }
+            }
         }
 }
--- src/test/kotlin/com/mattmik/rapira/variables/IndexedVariableTest.kt
+++ src/test/kotlin/com/mattmik/rapira/variables/IndexedVariableTest.kt
@@ -1,8 +1,12 @@
 package com.mattmik.rapira.variables
 
+import com.mattmik.rapira.errors.RapiraInvalidOperationError
+import com.mattmik.rapira.objects.Empty
 import com.mattmik.rapira.objects.RInteger
 import com.mattmik.rapira.objects.Text
 import com.mattmik.rapira.objects.toSequence
+import com.mattmik.rapira.objects.toText
+import io.kotest.assertions.throwables.shouldThrowUnit
 import io.kotest.core.spec.style.WordSpec
 import io.kotest.matchers.shouldBe
 
@@ -20,24 +24,55 @@ class IndexedVariableTest : WordSpec({
         }
     }
 
-    "set" should {
-        "update element at specified index" {
-            val sequence = listOf(
+    "set" When {
+        "base variable is text" should {
+            val textObject = "Hello, world!".toText()
+            val textVariable = SimpleVariable(textObject)
+
+            "update character at specified index" {
+                val indexedVariable = IndexedVariable(textVariable, RInteger(2))
+
+                indexedVariable.value shouldBe Text("e")
+                indexedVariable.value = Text("a")
+                indexedVariable.value shouldBe Text("a")
+                textVariable.value shouldBe "Hallo, world!".toText()
+            }
+
+            "throw error if index is non-text" {
+                val indexedVariable = IndexedVariable(textVariable, RInteger(2))
+                shouldThrowUnit<RapiraInvalidOperationError> {
+                    indexedVariable.value = Empty
+                }
+            }
+
+            "throw error if index is text with invalid length" {
+                val indexedVariable = IndexedVariable(textVariable, RInteger(2))
+                shouldThrowUnit<RapiraInvalidOperationError> {
+                    indexedVariable.value = Text("hello")
+                }
+            }
+        }
+
+        "base variable is sequence" should {
+            val sequenceObject = listOf(
                 Text("alpha"),
                 Text("beta"),
                 Text("gamma")
             ).toSequence()
-            val simpleVariable = SimpleVariable(sequence)
-            val indexedVariable = IndexedVariable(simpleVariable, RInteger(2))
+            val sequenceVariable = SimpleVariable(sequenceObject)
 
-            indexedVariable.value shouldBe Text("beta")
-            indexedVariable.value = Text("delta")
-            indexedVariable.value shouldBe Text("delta")
-            simpleVariable.value shouldBe listOf(
-                Text("alpha"),
-                Text("delta"),
-                Text("gamma")
-            ).toSequence()
+            "update element at specified index" {
+                val indexedVariable = IndexedVariable(sequenceVariable, RInteger(2))
+
+                indexedVariable.value shouldBe Text("beta")
+                indexedVariable.value = Text("delta")
+                indexedVariable.value shouldBe Text("delta")
+                sequenceVariable.value shouldBe listOf(
+                    Text("alpha"),
+                    Text("delta"),
+                    Text("gamma")
+                ).toSequence()
+            }
         }
     }
 })
