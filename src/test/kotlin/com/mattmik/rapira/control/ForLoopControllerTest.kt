4c1f7f80a41c101f900d89d2156309e0cd570c9f
Matthew Mikolay <mikolaym@yahoo.com>
2020-08-08 14:01:09 -0400

    Update ForLoopControllerTest

--- src/test/kotlin/com/mattmik/rapira/control/ForLoopControllerTest.kt
+++ src/test/kotlin/com/mattmik/rapira/control/ForLoopControllerTest.kt
@@ -9,6 +9,8 @@ import com.mattmik.rapira.util.Result
 import com.mattmik.rapira.variables.SimpleVariable
 import com.mattmik.rapira.variables.Variable
 import io.kotest.core.spec.style.WordSpec
+import io.kotest.matchers.booleans.shouldBeFalse
+import io.kotest.matchers.booleans.shouldBeTrue
 import io.mockk.every
 import io.mockk.mockk
 import io.mockk.verify
@@ -40,7 +42,163 @@ class ForLoopControllerTest : WordSpec({
         }
     }
 
-    // TODO Test isLoopActive
+    "isLoopActive" When {
+
+        "to expression is absent" should {
+            val fromInt = 10
+            val toValue = null
+
+            "return true" {
+                every { mockVariable.setValue(any()) } returns Result.Success(Unit)
+
+                ForLoopController(
+                    mockVariable,
+                    fromValue = fromInt.toRInteger(),
+                    toValue = toValue,
+                    stepValue = null,
+                    ctx = mockContext
+                ).isLoopActive().shouldBeTrue()
+            }
+        }
+
+        "to expression is specified and step is unspecified" should {
+            val stepValue = null
+
+            "return true until variable exceeds positive to expression" {
+                val fromInt = 10
+                val toInt = 12
+                val simpleVariable = SimpleVariable(Empty)
+
+                val loopController = ForLoopController(
+                    simpleVariable,
+                    fromValue = fromInt.toRInteger(),
+                    toValue = toInt.toRInteger(),
+                    stepValue = stepValue,
+                    ctx = mockContext
+                )
+
+                for (i in fromInt..toInt) {
+                    loopController.isLoopActive().shouldBeTrue()
+                    loopController.update()
+                }
+
+                loopController.isLoopActive().shouldBeFalse()
+            }
+
+            "return true until variable exceeds negative to expression" {
+                val fromInt = -10
+                val toInt = -8
+                val simpleVariable = SimpleVariable(Empty)
+
+                val loopController = ForLoopController(
+                    simpleVariable,
+                    fromValue = fromInt.toRInteger(),
+                    toValue = toInt.toRInteger(),
+                    stepValue = stepValue,
+                    ctx = mockContext
+                )
+
+                for (i in fromInt..toInt) {
+                    loopController.isLoopActive().shouldBeTrue()
+                    loopController.update()
+                }
+
+                loopController.isLoopActive().shouldBeFalse()
+            }
+        }
+
+        "to expression is specified and step is positive" should {
+            val stepInt = 2
+
+            "return true until variable exceeds positive to expression" {
+                val fromInt = 10
+                val toInt = 14
+                val simpleVariable = SimpleVariable(Empty)
+
+                val loopController = ForLoopController(
+                    simpleVariable,
+                    fromValue = fromInt.toRInteger(),
+                    toValue = toInt.toRInteger(),
+                    stepValue = stepInt.toRInteger(),
+                    ctx = mockContext
+                )
+
+                for (i in fromInt..toInt step stepInt) {
+                    loopController.isLoopActive().shouldBeTrue()
+                    loopController.update()
+                }
+
+                loopController.isLoopActive().shouldBeFalse()
+            }
+
+            "return true until variable exceeds negative to expression" {
+                val fromInt = -10
+                val toInt = -6
+                val simpleVariable = SimpleVariable(Empty)
+
+                val loopController = ForLoopController(
+                    simpleVariable,
+                    fromValue = fromInt.toRInteger(),
+                    toValue = toInt.toRInteger(),
+                    stepValue = stepInt.toRInteger(),
+                    ctx = mockContext
+                )
+
+                for (i in fromInt..toInt step stepInt) {
+                    loopController.isLoopActive().shouldBeTrue()
+                    loopController.update()
+                }
+
+                loopController.isLoopActive().shouldBeFalse()
+            }
+        }
+
+        "to expression is specified and step is negative" should {
+            val stepInt = -2
+
+            "return true until variable exceeds positive to expression" {
+                val fromInt = 18
+                val toInt = 14
+                val simpleVariable = SimpleVariable(Empty)
+
+                val loopController = ForLoopController(
+                    simpleVariable,
+                    fromValue = fromInt.toRInteger(),
+                    toValue = toInt.toRInteger(),
+                    stepValue = stepInt.toRInteger(),
+                    ctx = mockContext
+                )
+
+                for (i in fromInt downTo toInt step -stepInt) {
+                    loopController.isLoopActive().shouldBeTrue()
+                    loopController.update()
+                }
+
+                loopController.isLoopActive().shouldBeFalse()
+            }
+
+            "return true until variable exceeds negative to expression" {
+                val fromInt = -10
+                val toInt = -14
+                val simpleVariable = SimpleVariable(Empty)
+
+                val loopController = ForLoopController(
+                    simpleVariable,
+                    fromValue = fromInt.toRInteger(),
+                    toValue = toInt.toRInteger(),
+                    stepValue = stepInt.toRInteger(),
+                    ctx = mockContext
+                )
+
+                for (i in fromInt downTo toInt step -stepInt) {
+                    loopController.isLoopActive().shouldBeTrue()
+                    loopController.update()
+                }
+
+                loopController.isLoopActive().shouldBeFalse()
+            }
+        }
+    }
 
     "update" should {
         "increment variable with a default step of 1" {
