74d77a624400d2fd6e0d1569c0a7567cfe93fc5e
Matthew Mikolay <mikolaym@yahoo.com>
2020-08-15 13:01:36 -0400

    Add more error checking to RInteger

--- src/main/kotlin/com/mattmik/rapira/objects/RInteger.kt
+++ src/main/kotlin/com/mattmik/rapira/objects/RInteger.kt
@@ -28,32 +28,45 @@ data class RInteger(val value: Int) : RObject {
     override fun times(other: RObject) = when (other) {
         is RInteger -> RInteger(value * other.value).toSuccess()
         is Real -> Real(value * other.value).toSuccess()
-        is Text -> Text(other.value.repeat(value)).toSuccess()
-        is Sequence -> Sequence(arrayOfNulls<RObject>(value).flatMap { other.entries }).toSuccess()
+        is Text -> when {
+            value >= 0 -> Text(other.value.repeat(value)).toSuccess()
+            else -> Result.Error("Cannot multiply text by negative integer")
+        }
+        is Sequence -> when {
+            value >= 0 -> Sequence(arrayOfNulls<RObject>(value).flatMap { other.entries }).toSuccess()
+            else -> Result.Error("Cannot multiply sequence by negative integer")
+        }
         else -> super.times(other)
     }
 
     override fun div(other: RObject) = when (other) {
-        is RInteger ->
-            if (value % other.value == 0)
-                RInteger(value / other.value).toSuccess()
-            else Real(value.toDouble() / other.value).toSuccess()
-        is Real -> Real(value / other.value).toSuccess()
+        is RInteger -> when {
+            other.value == 0 -> Result.Error("Invalid division operation with zero value")
+            value % other.value == 0 -> RInteger(value / other.value).toSuccess()
+            else -> Real(value.toDouble() / other.value).toSuccess()
+        }
+        is Real -> when (other.value) {
+            0.0, -0.0 -> Result.Error("Invalid division operation with zero value")
+            else -> Real(value / other.value).toSuccess()
+        }
         else -> super.div(other)
     }
 
     override fun intDivide(other: RObject) = when (other) {
-        is RInteger -> {
-            if (other.value <= 0)
-                Result.Error("Cannot perform integer division with negative value")
-            else
-                RInteger(value / other.value).toSuccess()
+        is RInteger -> when {
+            other.value == 0 -> Result.Error("Invalid integer division operation with zero value")
+            other.value < 0 -> Result.Error("Invalid integer division operation with negative value")
+            else -> RInteger(value / other.value).toSuccess()
         }
         else -> super.intDivide(other)
     }
 
     override fun rem(other: RObject) = when (other) {
-        is RInteger -> RInteger(value % other.value).toSuccess()
+        is RInteger -> when {
+            other.value == 0 -> Result.Error("Invalid remainder operation with zero value")
+            other.value < 0 -> Result.Error("Invalid remainder operation with negative value")
+            else -> RInteger(value % other.value).toSuccess()
+        }
         else -> super.rem(other)
     }
 
--- src/test/kotlin/com/mattmik/rapira/objects/RIntegerTest.kt
+++ src/test/kotlin/com/mattmik/rapira/objects/RIntegerTest.kt
@@ -1,7 +1,19 @@
 package com.mattmik.rapira.objects
 
+import com.mattmik.rapira.CONST_YES
 import io.kotest.core.spec.style.WordSpec
+import io.kotest.data.forAll
+import io.kotest.data.row
+import io.kotest.property.Arb
+import io.kotest.property.arbitrary.int
+import io.kotest.property.arbitrary.negativeInts
+import io.kotest.property.arbitrary.positiveDoubles
+import io.kotest.property.arbitrary.positiveInts
+import io.kotest.property.arbitrary.string
 import io.kotest.property.checkAll
+import kotlin.math.exp
+import kotlin.math.ln
+import kotlin.math.pow
 
 class RIntegerTest : WordSpec({
     "plus" should {
@@ -14,6 +26,19 @@ class RIntegerTest : WordSpec({
         "succeed with real when given real" {
             checkAll<Int, Double> { a, b ->
                 RInteger(a) + Real(b) shouldSucceedWith Real(a + b)
+            }
+        }
+
+        "error when given other types" {
+            forAll(
+                row(Empty),
+                row(Procedure()),
+                row(Function()),
+                row(CONST_YES),
+                row(Text("hello")),
+                row(Sequence())
+            ) { obj ->
+                (RInteger(123) + obj).shouldError()
             }
         }
     }
@@ -30,12 +55,251 @@ class RIntegerTest : WordSpec({
                 RInteger(a) - Real(b) shouldSucceedWith Real(a - b)
             }
         }
+
+        "error when given other types" {
+            forAll(
+                row(Empty),
+                row(Procedure()),
+                row(Function()),
+                row(CONST_YES),
+                row(Text("hello")),
+                row(Sequence())
+            ) { obj ->
+                (RInteger(123) - obj).shouldError()
+            }
+        }
     }
 
     "negate" should {
         "succeed with integer" {
             checkAll<Int> { num ->
                 RInteger(num).negate() shouldSucceedWith RInteger(-num)
+            }
+        }
+    }
+
+    "times" should {
+        "succeed with integer when given integer" {
+            checkAll<Int, Int> { a, b ->
+                RInteger(a) * RInteger(b) shouldSucceedWith RInteger(a * b)
+            }
+        }
+
+        "succeed with real when given real" {
+            checkAll<Int, Double> { a, b ->
+                RInteger(a) * Real(b) shouldSucceedWith Real(a * b)
+            }
+        }
+
+        "succeed with text when given text and non-negative int" {
+            checkAll<String> { str ->
+                RInteger(0) * Text(str) shouldSucceedWith Text("")
+            }
+            checkAll(Arb.positiveInts(max = 100), Arb.string()) { num, str ->
+                RInteger(num) * Text(str) shouldSucceedWith str.repeat(num).toText()
+            }
+        }
+
+        "error when given text and negative int" {
+            checkAll(Arb.negativeInts(), Arb.string()) { num, str ->
+                (RInteger(num) * Text(str)).shouldError()
+            }
+        }
+
+        "succeed with empty sequence when given sequence and zero" {
+            val sequence = listOf(
+                1.toRInteger(),
+                "Hello, world!".toText(),
+                2.toRInteger(),
+                "This is a test.".toText()
+            ).toSequence()
+
+            RInteger(0) * sequence shouldSucceedWith emptyList<RObject>().toSequence()
+        }
+
+        "succeed with sequence when given sequence and positive int" {
+            val objList = listOf(
+                1.toRInteger(),
+                "Hello, world!".toText(),
+                2.toRInteger(),
+                "This is a test.".toText()
+            )
+
+            val expectedList = mutableListOf<RObject>()
+            repeat(3) { expectedList.addAll(objList) }
+            RInteger(3) * objList.toSequence() shouldSucceedWith expectedList.toSequence()
+        }
+
+        "error when given sequence and negative int" {
+            val sequence = listOf(
+                1.toRInteger(),
+                "Hello, world!".toText(),
+                2.toRInteger(),
+                "This is a test.".toText()
+            ).toSequence()
+
+            checkAll(Arb.negativeInts()) { num ->
+                (RInteger(num) * sequence).shouldError()
+            }
+        }
+
+        "error when given other types" {
+            forAll(
+                row(Empty),
+                row(Procedure()),
+                row(Function()),
+                row(CONST_YES)
+            ) { obj ->
+                (RInteger(123) * obj).shouldError()
+            }
+        }
+    }
+
+    "div" should {
+        "succeed when given positive integer" {
+            checkAll(Arb.int(), Arb.positiveInts()) { a, b ->
+                RInteger(a) / RInteger(b) shouldSucceedWith
+                    if (a % b == 0) RInteger(a / b) else Real(a.toDouble() / b)
+            }
+        }
+
+        "succeed when given negative integer" {
+            checkAll(Arb.int(), Arb.negativeInts()) { a, b ->
+                RInteger(a) / RInteger(b) shouldSucceedWith
+                    if (a % b == 0) RInteger(a / b) else Real(a.toDouble() / b)
+            }
+        }
+
+        "error when given integer zero" {
+            checkAll<Int> { num ->
+                (RInteger(num) / RInteger(0)).shouldError()
+            }
+        }
+
+        "succeed with real when given positive real" {
+            checkAll(Arb.int(), Arb.positiveDoubles()) { a, b ->
+                RInteger(a) / Real(b) shouldSucceedWith Real(a / b)
+            }
+        }
+
+        "succeed with real when given negative real" {
+            checkAll(Arb.int(), Arb.positiveDoubles()) { a, b ->
+                RInteger(a) / Real(-b) shouldSucceedWith Real(a / -b)
+            }
+        }
+
+        "error when given real zero" {
+            checkAll<Int> { num ->
+                (RInteger(num) / Real(0.0)).shouldError()
+                (RInteger(num) / Real(-0.0)).shouldError()
+            }
+        }
+
+        "error when given other types" {
+            forAll(
+                row(Empty),
+                row(Procedure()),
+                row(Function()),
+                row(CONST_YES),
+                row(Text("hello")),
+                row(Sequence())
+            ) { obj ->
+                (RInteger(123) / obj).shouldError()
+            }
+        }
+    }
+
+    "intDivide" should {
+        "error with zero" {
+            checkAll<Int> { num ->
+                RInteger(num).intDivide(0.toRInteger()).shouldError()
+            }
+        }
+
+        "error with negative integer" {
+            checkAll(Arb.int(), Arb.negativeInts()) { a, b ->
+                a.toRInteger().intDivide(b.toRInteger()).shouldError()
+            }
+        }
+
+        "succeed with integer when given positive integer" {
+            checkAll(Arb.int(), Arb.positiveInts()) { a, b ->
+                a.toRInteger().intDivide(b.toRInteger()) shouldSucceedWith RInteger(a / b)
+            }
+        }
+
+        "error when given other types" {
+            forAll(
+                row(Empty),
+                row(Real(123.0)),
+                row(Procedure()),
+                row(Function()),
+                row(CONST_YES),
+                row(Text("hello")),
+                row(Sequence())
+            ) { obj ->
+                RInteger(123).intDivide(obj).shouldError()
+            }
+        }
+    }
+
+    "rem" should {
+        "error with zero" {
+            checkAll<Int> { num ->
+                (RInteger(num) % RInteger(0)).shouldError()
+            }
+        }
+
+        "error with negative integer" {
+            checkAll(Arb.int(), Arb.negativeInts()) { a, b ->
+                (RInteger(a) % RInteger(b)).shouldError()
+            }
+        }
+
+        "succeed with integer when given positive integer" {
+            checkAll(Arb.int(), Arb.positiveInts()) { a, b ->
+                RInteger(a) % RInteger(b) shouldSucceedWith RInteger(a % b)
+            }
+        }
+
+        "error when given other types" {
+            forAll(
+                row(Empty),
+                row(Real(123.0)),
+                row(Procedure()),
+                row(Function()),
+                row(CONST_YES),
+                row(Text("hello")),
+                row(Sequence())
+            ) { obj ->
+                (RInteger(123) % obj).shouldError()
+            }
+        }
+    }
+
+    "power" should {
+        "succeed with integer when given integer" {
+            checkAll<Int, Int> { a, b ->
+                a.toRInteger().power(b.toRInteger()) shouldSucceedWith a.toDouble().pow(b).toInt().toRInteger()
+            }
+        }
+
+        "succeed with real when given real" {
+            checkAll<Int, Double> { a, b ->
+                a.toRInteger().power(b.toReal()) shouldSucceedWith exp(ln(a.toDouble()) * b).toReal()
+            }
+        }
+
+        "error when given other types" {
+            forAll(
+                row(Empty),
+                row(Procedure()),
+                row(Function()),
+                row(CONST_YES),
+                row(Text("hello")),
+                row(Sequence())
+            ) { obj ->
+                RInteger(123).power(obj).shouldError()
             }
         }
     }
@@ -50,6 +314,19 @@ class RIntegerTest : WordSpec({
         "compare when given real number" {
             checkAll<Int, Double> { a, b ->
                 a.toRInteger().compare(b.toReal()) shouldSucceedWith a.compareTo(b)
+            }
+        }
+
+        "error when given other types" {
+            forAll(
+                row(Empty),
+                row(Procedure()),
+                row(Function()),
+                row(CONST_YES),
+                row(Text("hello")),
+                row(Sequence())
+            ) { obj ->
+                RInteger(123).compare(obj).shouldError()
             }
         }
     }
